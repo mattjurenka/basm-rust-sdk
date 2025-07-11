bdc71f41997578ba29a617f2f182ce179451e167
mattjurenka <matt.jurenka@comcast.net>
2025-07-11 12:57:21 -0600

    working with rust wasm

--- integration-test/src/lib.rs
+++ integration-test/src/lib.rs
@@ -1,12 +1,6 @@
 use basm_rust_sdk::leak_to_shared_memory;
 
-
-
 #[no_mangle]
 pub extern "C" fn hello_world(input_ptr: u64, secret_ptr: u64) -> u64 {
-    return 0;
-    //return leak_to_shared_memory("Hello World.".as_bytes());
+    return leak_to_shared_memory("Hello from Rust -> Wasm on Blocky! 🦀".as_bytes());
 }
-
-#[no_mangle]
-pub extern "C" fn main() {}
